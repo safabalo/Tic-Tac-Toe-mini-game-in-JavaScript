7175af9d9efa670b424e88737c76504e931916e0
safabalo <safaabalha58@gmail.com>
2022-09-23 16:14:03 +0100

    v0.01 without winner part

--- src/javaScript/game.js
+++ src/javaScript/game.js
@@ -1,9 +1,26 @@
 /*Timer for the game */
+localStorage.clear();
 clicked = false;
 const timer = document.getElementById('countdown');
 const starter = document.getElementById('start');
 let timeMinute = 10;
 let time = timeMinute*60;
+const cells = document.querySelectorAll('div');
+possiBilities = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6],
+];
+const circle = 'O';
+const x = 'X';
+let circleTour;
 function counter(){
     const setting = setInterval(function () {
         countDown()
@@ -24,3 +41,30 @@ function countDown(){
     }
     
 }
+// console.log(possiBilities)
+// console.log(typeof(cells))
+// window.localStorage['fontSize']='20px';
+cells.forEach((cell, index )=> { 
+        cell.addEventListener('click', function(){
+           const currentClass = circleTour ? circle : x
+           localStorage.setItem(JSON.stringify(index),currentClass)
+           console.log(localStorage)
+            marks(cell,currentClass)
+        },{once:true})    
+});
+//Adding the X or O
+function marks(cell, currentClass){
+cell.innerText = currentClass
+switchTurn()
+}
+// Checking the player
+function switchTurn(){
+    circleTour=!circleTour
+}
+// function winner(currentClass){
+//     return possiBilities.some(posibilite => {
+//         return posibilite.every(index =>{
+//             return cells[index].cell.contains(currentClass)
+//         })
+//     })
+// }
